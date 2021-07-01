44d7ee787e03c24054095a3efd151ba93dfea999
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-01 18:34:59 +0900

    rails+deviseの日本語化

--- src/config/application.rb
+++ src/config/application.rb
@@ -18,5 +18,10 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # deviseの日本語化
+    config.i18n.default_locale = :ja
+    # タイムゾーンの変更
+    config.time_zone = 'Asia/Tokyo'
   end
 end
