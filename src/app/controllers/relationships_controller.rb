51ff26c76de4191199725f556fca3e0707508cda
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-12 21:27:46 +0900

    フォロー機能をAjax化＋ボタンをuser詳細ページへ移動

--- src/app/controllers/relationships_controller.rb
+++ src/app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])# views/relationships/create.js.erbへ送るため
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to users_path
   end
 
   def destroy
+    @user = User.find(params[:user_id])# views/relationships/destroy.js.erbへ送るため
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
   end
 end
