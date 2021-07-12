ed587a9501be1cf6608421488ec72a7dde2b5e00
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-12 11:29:58 +0900

    user#show作成 お気に入り記事一覧にajax適用

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -6,5 +6,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+    @favorite_articles = @user.favorite_articles
   end
 end
