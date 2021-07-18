b6d5ffe99a727044fe2c434ec54fa954a66b00f1
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-18 15:02:20 +0900

    user 自己紹介文機能追加

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
   # CSRF対策
   protect_from_forgery with: :exception
-  # user新規作成の項目追加
+  # user新規作成・編集の項目追加
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  # user新規作成用
+  # user新規作成・編集用
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :introduce])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:introduce])
   end
 end
