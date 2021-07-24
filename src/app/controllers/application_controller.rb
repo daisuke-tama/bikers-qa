12c3c1834d4be3fd92cf296f14efc25b45815365
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-24 19:50:10 +0900

    ヘッダーに記事検索機能追加

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_article_q
   # CSRF対策
   protect_from_forgery with: :exception
+
+  # 記事検索用
+  def search
+    @results = @q.result
+  end
+
+  def set_article_q
+    @q = Article.ransack(params[:q])
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
 
   resources :articles do
     resource :favorites, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
