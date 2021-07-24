080df40a6001d4a5714c8e02c4975979ee08eec0
daisuke-tama <smithhearth.dr@gmail.com>
2021-07-24 13:52:03 +0900

    トップページにお気に入り数とフォロー数のランキングを追加

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @favorite_ranking = User.find(Relationship.group(:follower_id).order('count(follower_id) desc').limit(10).pluck(:follower_id))
     @articles = Article.order(id: :desc).limit(10)
+    @article_ranking = Article.find(Favorite.group(:article_id).order('count(article_id) desc').limit(10).pluck(:article_id))
     # TagモデルにTagMapモデルのtag_id（tag_id毎にグループ化＋多い順にソート＋取得数１０＋Tagモデルのidとするためにtag_idを数値で取得）を代入
     @tag_ranking = Tag.find(TagMap.group(:tag_id).order('count(tag_id) desc').limit(10).pluck(:tag_id))
   end
